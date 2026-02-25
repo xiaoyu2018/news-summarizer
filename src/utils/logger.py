37e00058f347fcc3c3028299dee58604df41e300
Xavier <861900161@qq.com>
2026-02-25 20:55:40 +0800

    refactor main.py and update logger format

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -25,7 +25,7 @@ def setup_logger(
         handler.setLevel(getattr(logging, level.upper()))
 
         formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            "%(asctime)s [%(levelname)s] %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
         )
 
